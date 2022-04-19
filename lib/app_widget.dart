f625f13cbb2e4f0294145783918c13ca9c396359
Paulo <paulohdef1@gmail.com>
2022-04-19 18:29:39 -0300

    implementado rota de cadastro

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -4,41 +4,45 @@ import 'package:conectepet/app_controller.dart';
 import 'home_page.dart';
 import 'login_page.dart';
 import 'pets_page.dart';
-
-
+import 'cadastroTutor_page.dart';
 
 class AppWidget extends StatelessWidget {
-  
   final String title;
 
   const AppWidget({Key? key, required this.title}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return AnimatedBuilder(
-      animation: AppController.instance,
-      builder: (context, child){
-        return MaterialApp(
-          theme: ThemeData(
-            primarySwatch: Colors.red,
-            colorScheme: ColorScheme(
+        animation: AppController.instance,
+        builder: (context, child) {
+          return MaterialApp(
+            theme: ThemeData(
+              primarySwatch: Colors.red,
+              colorScheme: ColorScheme(
+                brightness: Brightness.light,
+                primary: Color.fromARGB(255, 246, 146, 30),
+                onPrimary: Color.fromARGB(255, 0, 0, 0),
+                secondary: Color.fromARGB(255, 246, 146, 30),
+                onSecondary: Color.fromARGB(255, 0, 0, 0),
+                background: Color.fromARGB(255, 0, 0, 0),
+                error: Color.fromARGB(255, 94, 255, 0),
+                onBackground: Color.fromARGB(255, 255, 255, 255),
+                onError: Color.fromARGB(255, 0, 81, 255),
+                onSurface: Colors.grey,
+                surface: Color.fromARGB(255, 255, 0, 0),
+              ),
               brightness: Brightness.light,
-              primary: Color.fromARGB(255, 1, 8, 37),
-              onPrimary: Colors.white,
-              secondary: Color.fromARGB(255, 3, 7, 59),
-              onSecondary: Color.fromARGB(255, 255, 255, 255), background: Color.fromARGB(255, 255, 0, 0), error: Color.fromARGB(255, 94, 255, 0), onBackground:Color.fromARGB(255, 255, 255, 255), onError:Color.fromARGB(255, 0, 81, 255), onSurface: Colors.grey, surface: Color.fromARGB(255, 255, 0, 0),
             ),
-            brightness: AppController.instance.isDarkTheme ?  Brightness.light : Brightness.dark,
-            
-            ),
-          initialRoute: '/',
-          routes: {
-            '/': (context)=> LoginPage(),
-            '/home': (context)=> HomePage(),
-            '/pets': (context)=>PetsPage()
-          },
-        );
-      });
+            initialRoute: '/',
+            routes: {
+              '/': (context) => LoginPage(),
+              '/home': (context) => HomePage(),
+              '/pets': (context) => PetsPage(),
+              '/cadastro': (context) => CadastroTutorPage()
+            },
+          );
+        });
   }
 }
