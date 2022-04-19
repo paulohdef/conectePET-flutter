a4bd82f87e9fe7d6c09038fd7a1c29649420aa45
Paulo <paulohdef1@gmail.com>
2022-04-19 18:29:00 -0300

    modificacao no estilo da view pets

--- lib/pets_page.dart
+++ lib/pets_page.dart
@@ -1,51 +1,72 @@
 import 'package:flutter/material.dart';
 
 class PetsPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body : ListaTransferencia(),
-      appBar : AppBar(title: Text('Pets Cadastrados') ),
+      backgroundColor: Colors.amberAccent,
+      body: ListaTransferencia(),
+      appBar: AppBar(
+        title: Text('Pets Cadastrados'),
+        actions: [
+        IconButton(onPressed: (){
+          Navigator.of(context).pushReplacementNamed('/');
+        }, icon: Icon(Icons.logout))
+
+        ],),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {  },
+        onPressed: () {},
         child: Icon(Icons.add),
       ),
     );
   }
 }
 
-
-class ListaTransferencia extends StatelessWidget{
+class ListaTransferencia extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Column(
-      children: [
-        ItemTransferencia(Transferencia("Jorge", "Pastor-Alemão")),
-        ItemTransferencia(Transferencia("Romeu", "Yorkshire")),
-        ItemTransferencia(Transferencia("Doguinho", "Labrador")),
-        ItemTransferencia(Transferencia("Jeremias", "Siamês")),
-        SizedBox(
-                  height: 50,
-                ),
-                ElevatedButton(
-                  
-                  onPressed: (){
-                      Navigator.of(context).pushReplacementNamed('/');
-                  },
-                  child: Text('Sair'),
-                  style: ElevatedButton.styleFrom(
-                    primary: Color.fromARGB(255, 1, 9, 31),
-                  ),
-                )
-      ],
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          ItemTransferencia(Transferencia("Jorge", "Pastor Alemão")),
+          ItemTransferencia(Transferencia("Romeu", "Yorkshire")),
+          ItemTransferencia(Transferencia("Doguinho", "Labrador")),
+          ItemTransferencia(Transferencia("Thor", "Bull Terrier")),
+          ItemTransferencia(Transferencia("Bob", "Pug")),
+          ItemTransferencia(Transferencia("Fred", "Poodle")),
+          ItemTransferencia(Transferencia("Billy", "Chihuaha")),
+          ItemTransferencia(Transferencia("Marley", "Boxer")),
+          ItemTransferencia(Transferencia("Pingo", "Husky Siberiano")),
+          ItemTransferencia(Transferencia("Bidu", "Golden Retriever")),
+          SizedBox(
+            height: 20,
+          ),
+          SizedBox(
+            width: double.infinity,
+            height: 50.0,
+            child: ElevatedButton.icon(
+              onPressed: () {
+                Navigator.of(context).pushReplacementNamed('/');
+              },
+              icon: Icon(Icons.logout),
+              label: Text('Sair'),
+              style: ElevatedButton.styleFrom(
+                primary: Color.fromARGB(255, 246, 146, 30),
+                shape: StadiumBorder(),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+        ],
+      ),
     );
   }
 }
 
-class ItemTransferencia extends StatelessWidget{
-
+class ItemTransferencia extends StatelessWidget {
   final Transferencia _transferencia;
 
   ItemTransferencia(this._transferencia);
@@ -53,14 +74,16 @@ class ItemTransferencia extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Card(
+      elevation: 0,
+      color: Colors.transparent,  
       child: ListTile(
         leading: Icon(Icons.pets),
         title: Text(_transferencia.nome),
         subtitle: Text(_transferencia.raca),
+        
       ),
     );
   }
-
 }
 
 class Transferencia {
